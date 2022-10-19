7b653259265edd1d1f0b11f831577f40e9662248
feh-franc0 <fernandofv1110@gmail.com>
2022-10-18 22:55:28 -0300

    Criando rotas e compartilhando contexto~

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/authContext";
+import { Login } from "./pages/Login";
 import { Signup } from "./pages/Sininup";
+import { UserProfile } from "./pages/UserProfile";
 
 function App() {
   return (
     <AuthProvider>
-      <Signup />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/" element={<UserProfile />}></Route>
+          <Route
+            path="*"
+            element={
+              <div>
+                <h1>Rota não encontrada</h1>
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
